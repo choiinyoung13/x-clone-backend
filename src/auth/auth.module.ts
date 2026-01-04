2703890da7a8c7a40866f8494971aee05b2d6e7e
chldls153@naver.com <chldls153@gmail.com>
2026-01-05 02:42:36 +0900

    feat: UsersService 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
-import {Module} from '@nestjs/common';
-import {LocalSerializer} from './local.serializer';
-import {LocalStrategy} from './local.strategy';
-import {PassportModule} from '@nestjs/passport';
-import {UsersService} from '../apis/users/users.service';
+import { Module } from '@nestjs/common';
+import { LocalSerializer } from './local.serializer';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { UsersService } from '../apis/users/users.service';
+import { UploadModule } from '../common/upload/upload.module';
 
 @Module({
   imports: [
     PassportModule.register({ session: true }),
+    UploadModule,
   ],
   providers: [LocalSerializer, LocalStrategy, UsersService],
 })
-export class AuthModule {}
+export class AuthModule { }
